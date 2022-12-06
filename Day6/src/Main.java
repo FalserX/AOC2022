c2b695a7c1bb265d43ebe76e463ac8e3db8e3cb5
William Malenfant (FalserX) <22843250+FalserX@users.noreply.github.com>
2022-12-06 10:23:14 -0500

    Day 6 part 2

--- Day6/src/Main.java
+++ Day6/src/Main.java
@@ -9,11 +9,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> finalResult = new ArrayList<>();
-        int finalResult2 = 0;
+        List<Integer> finalResult2 = new ArrayList<>();
         Path currentDirectoryPath = Paths.get("").toAbsolutePath();
         List<String> lines;
         try {
             int result;
+            int result2;
             lines = Files.readAllLines(
                     Path.of(currentDirectoryPath + "/out/production/Day6/input.txt"),
                     StandardCharsets.UTF_8);
@@ -23,6 +24,13 @@ public class Main {
                     result = getFirstMarkerPositionWith4Characters(lineChar, i);
                     if (result != -1) {
                         finalResult.add(result);
+                        break;
+                    }
+                }
+                for (int i = 0; i < lineChar.length - 14;i++) {
+                    result2 = getFirstMarkerPositionWith14Characters(lineChar, i);
+                    if (result2 != -1) {
+                        finalResult2.add(result2);
                         break;
                     }
                 }
@@ -43,6 +51,51 @@ public class Main {
         if (firstPos != secondPos && firstPos != thirdPos && firstPos != fourthPos &&
                 secondPos != thirdPos && secondPos != fourthPos && thirdPos != fourthPos) {
             return startPos + 4;
+        }
+        return -1;
+    }
+    public static int getFirstMarkerPositionWith14Characters(char[] lineChar, int startPos) {
+        char firstPos = lineChar[startPos];
+        char secondPos = lineChar[startPos + 1];
+        char thirdPos = lineChar[startPos + 2];
+        char fourthPos = lineChar[startPos + 3];
+        char fifthPos = lineChar[startPos + 4];
+        char sixthPos = lineChar[startPos + 5];
+        char seventhPos = lineChar[startPos + 6];
+        char eighthPos = lineChar[startPos + 7];
+        char ninthPos = lineChar[startPos + 8];
+        char tenthPos = lineChar[startPos + 9];
+        char eleventhPos = lineChar[startPos + 10];
+        char twelvethPos = lineChar[startPos + 11];
+        char thirteenthPos = lineChar[startPos + 12];
+        char fourteenthPos = lineChar[startPos + 13];
+
+        if (firstPos != secondPos && firstPos != thirdPos && firstPos != fourthPos && firstPos != fifthPos &&
+                firstPos != sixthPos && firstPos != seventhPos && firstPos != eighthPos && firstPos != ninthPos &&
+                firstPos != tenthPos && firstPos != eleventhPos && firstPos != twelvethPos && firstPos != thirteenthPos
+                && firstPos != fourteenthPos && secondPos != thirdPos && secondPos != fourthPos &&
+                secondPos != fifthPos && secondPos != sixthPos && secondPos != seventhPos && secondPos != eighthPos &&
+                secondPos != ninthPos && secondPos != tenthPos && secondPos != eleventhPos && secondPos != twelvethPos &&
+                secondPos != thirteenthPos && secondPos != fourteenthPos && thirdPos != fourthPos && thirdPos != fifthPos
+                && thirdPos != sixthPos && thirdPos != seventhPos && thirdPos != eighthPos && thirdPos != ninthPos &&
+                thirdPos != tenthPos && thirdPos != eleventhPos && thirdPos != twelvethPos && thirdPos != thirteenthPos &&
+                thirdPos != fourteenthPos && fourthPos != fifthPos && fourthPos != sixthPos && fourthPos != seventhPos &&
+                fourthPos != eighthPos && fourthPos != ninthPos && fourthPos != tenthPos && fourthPos != eleventhPos &&
+                fourthPos != twelvethPos && fourthPos != thirteenthPos && fourthPos != fourteenthPos &&
+                fifthPos != sixthPos && fifthPos != seventhPos && fifthPos != eighthPos && fifthPos != ninthPos &&
+                fifthPos != tenthPos && fifthPos != eleventhPos && fifthPos != twelvethPos && fifthPos != thirteenthPos
+                && fifthPos != fourteenthPos && sixthPos != seventhPos && sixthPos != eighthPos && sixthPos != ninthPos
+                && sixthPos != tenthPos && sixthPos != eleventhPos && sixthPos != twelvethPos && sixthPos != thirteenthPos
+                && sixthPos != fourteenthPos && seventhPos != eighthPos && seventhPos != ninthPos && seventhPos != tenthPos
+                && seventhPos != eleventhPos && seventhPos != twelvethPos && seventhPos != thirteenthPos &&
+                seventhPos != fourteenthPos && eighthPos != ninthPos && eighthPos != tenthPos && eighthPos != eleventhPos
+                && eighthPos != twelvethPos && eighthPos != thirteenthPos && eighthPos != fourteenthPos &&
+                ninthPos != tenthPos && ninthPos != eleventhPos && ninthPos != twelvethPos && ninthPos != thirteenthPos &&
+                ninthPos != fourteenthPos && tenthPos != eleventhPos && tenthPos != twelvethPos && tenthPos != thirteenthPos
+                && tenthPos != fourteenthPos && eleventhPos != twelvethPos && eleventhPos != thirteenthPos &&
+                eleventhPos != fourteenthPos && twelvethPos != thirteenthPos && twelvethPos != fourteenthPos &&
+                thirteenthPos != fourteenthPos) {
+            return startPos + 14;
         }
         return -1;
     }
